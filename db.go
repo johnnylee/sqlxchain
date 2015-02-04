8e06a3728c9e951ee7fb81adbdbfbb439a335c4c
johnl <johnl@johnnylee.orgm>
2015-02-04 17:18:12 -0600

    Added the wrapper.

---
+++ db.go
@@ -0,0 +1,128 @@
+package sqlxchain
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/johnnylee/util"
+)
+
+var log = util.NewPrefixLogger("SqlxChain")
+
+// SqlxChain is a thin wrapper around a sqlx.DB.
+type SqlxChain struct {
+	Db *sqlx.DB
+}
+
+func New(driver, dns string) (*SqlxChain, error) {
+	var err error
+
+	sc := new(SqlxChain)
+	if sc.Db, err = sqlx.Open("mysql", dns); err != nil {
+		return nil, err
+	}
+
+	return sc, nil
+}
+
+func (sc *SqlxChain) Context() *DbContext {
+	ctx := new(DbContext)
+	ctx.db = sc.Db
+	return ctx
+}
+
+type DbContext struct {
+	db     *sqlx.DB
+	tx     *sqlx.Tx
+	err    error
+	result sql.Result
+}
+
+func (d *DbContext) Begin() *DbContext {
+	if d.err != nil {
+		return d
+	}
+	d.tx, d.err = d.db.Beginx()
+	return d
+}
+
+func (d *DbContext) Exec(query string, args ...interface{}) *DbContext {
+	if d.err != nil {
+		return d
+	}
+	if d.tx != nil {
+		d.result, d.err = d.tx.Exec(query, args...)
+	} else {
+		d.result, d.err = d.db.Exec(query, args...)
+	}
+	return d
+}
+
+func (d *DbContext) Get(
+	dest interface{}, query string, args ...interface{}) *DbContext {
+	if d.err != nil {
+		return d
+	}
+	if d.tx != nil {
+		d.err = d.tx.Get(dest, query, args...)
+	} else {
+		d.err = d.db.Get(dest, query, args...)
+	}
+	return d
+}
+
+func (d *DbContext) Select(
+	dest interface{}, query string, args ...interface{}) *DbContext {
+	if d.err != nil {
+		return d
+	}
+	if d.tx != nil {
+		d.err = d.tx.Select(dest, query, args...)
+	} else {
+		d.err = d.db.Select(dest, query, args...)
+	}
+	return d
+}
+
+func (d *DbContext) LastInsertId(id *int64) *DbContext {
+	if d.err != nil {
+		return d
+	}
+	*id, d.err = d.result.LastInsertId()
+	return d
+}
+
+func (d *DbContext) RowsAffected(n *int64) *DbContext {
+	if d.err != nil {
+		return d
+	}
+	*n, d.err = d.result.RowsAffected()
+	return d
+}
+
+func (d *DbContext) LogErr(msg string, args ...interface{}) *DbContext {
+	if d.err != nil {
+		log.Err(d.err, msg, args...)
+	}
+	return d
+}
+
+func (d *DbContext) Commit() *DbContext {
+	defer func() {
+		d.tx = nil
+	}()
+
+	if d.err != nil {
+		if err := d.tx.Rollback(); err != nil {
+			log.Err(err, "When rolling back transaction")
+		}
+		return d
+	}
+
+	d.err = d.tx.Commit()
+	return d
+}
+
+func (d *DbContext) Err() error {
+	return d.err
+}
